c53a1cc83af43b147a5388d280ea5cd8c05e7d9e
hardikale <mehardikale@gmail.com>
2024-11-05 20:55:34 +0545

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        raleway: ['Raleway', 'sans-serif'],
+        quicksand: ['Quicksand', 'san-serif'],  
+      }
+    },
+  },
+  plugins: [],
+}
